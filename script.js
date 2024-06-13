d33dd9f724ff0468c73f7d455256d2b9b8406792
JohnnyUtah666 <porksloppp@gmail.com>
2024-06-13 14:58:26 -0700

    Within player objects add logic that prevents them from placing their marker inside a square that has already been claimed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-    const board = ["", "", "", "", "", "", "", "", ""]
+    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     return {board};
 })();
 
@@ -7,7 +7,11 @@ const playerOne = (function () {
     let name = "Player One";
     const marker = 1;
     function placeMarker(num) {
-        gameBoard.board.splice(num, 1, 1);
+        if (gameBoard.board[num] == 0){
+            gameBoard.board.splice(num, 1, 1);
+        } else if (gameBoard.board[num].value == 1 || gameBoard.board[num].value ==2) {
+            return;
+        }
     }
     return {placeMarker}
 })();
@@ -16,13 +20,28 @@ const playerTwo = (function () {
     let name = "Player Two";
     const marker =2;
     function placeMarker(num) {
+       if (gameBoard.board[num] == 0) {
        gameBoard.board.splice(num, 1, 2);
+       } else if (gameBoard.board[num].value == 1 || gameBoard.board[num].value == 2) {
+        return;
+       }
     }
     return {placeMarker}
 })();
 
+function game () {
+    let isTurn = playerOne;
+
+};
+
 playerOne.placeMarker(6);
 playerTwo.placeMarker(4);
+playerOne.placeMarker(3);
+playerTwo.placeMarker(0);
+playerOne.placeMarker(1);
 
 console.log(gameBoard.board[4]);
 console.log(gameBoard.board[6]);
+console.log(gameBoard.board[3]);
+console.log(gameBoard.board[0]);
+console.log(gameBoard.board[1]);
